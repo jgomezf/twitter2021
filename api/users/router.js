d1ae13a85770e25b009836bc7cfb261daba53a16
Juan David Gomez <fogodaju@gmail.com>
2021-04-15 19:21:53 -0500

    revisión de codigo con eslint

--- api/users/router.js
+++ api/users/router.js
@@ -1,22 +1,25 @@
 const express = require('express');
-const { list, create, update, login, remove } = require('./controller')
+const {
+  list, create, update, login, remove,
+} = require('./controller');
 const { logger } = require('../middlewares/logger');
 const { validatUser, validateLogin } = require('../middlewares/validator');
 const { authenticator } = require('../middlewares/authenticator');
 const { usersAuthorization } = require('../middlewares/authorization');
+
 const router = express.Router();
 
 router.use(logger);
 
 router.route('/')
-    .get(authenticator, list)
-    .delete(authenticator, usersAuthorization, remove)
-    .post(validatUser, create);
+  .get(authenticator, list)
+  .delete(authenticator, usersAuthorization, remove)
+  .post(validatUser, create);
 
 router.route('/login')
-    .post(validateLogin, login);
+  .post(validateLogin, login);
 
 router.route('/:username')
-    .put(update);
-    
+  .put(update);
+
 module.exports = router;
--- api/weather/router.js
+++ api/weather/router.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { get } = require('./controller')
+const { get } = require('./controller');
 const { logger } = require('../middlewares/logger');
+
 const router = express.Router();
 
 router.use(logger);
 
 router.route('/:city')
-    .get(get);
-    
+  .get(get);
+
 module.exports = router;
