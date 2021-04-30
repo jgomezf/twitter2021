2b6493e70a3e38d13eaab7d324d1439d8724b7c5
Juan David Gomez <fogodaju@gmail.com>
2021-04-29 21:01:37 -0500

    implementar seeds

---
+++ seeds/data.js
@@ -0,0 +1,18 @@
+const users = [
+  {
+    name: "Admon Tweeter",
+    username: "admin",
+    email: "admin@email.com",
+    role: "admin",
+  },
+  // ,
+  // {
+  //   name: "Persona2 Apellido2",
+  //   username: "persona2",
+  //   email: "persona2@email.com",
+  // },
+];
+
+const tweets = [{}];
+
+module.exports = { users, tweets };
